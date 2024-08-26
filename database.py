d56fc027c6c6ce8d21516d9456af9145e46618fe
naromu <natyrm15173444@gmail.com>
2024-08-25 21:44:19 -0500

    REFACTOR(env): update environment variables to use PG* instead of POSTGRES_*

--- database.py
+++ database.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Obtener los datos de conexión de las variables de entorno
-DB_HOST = os.getenv("POSTGRES_HOST")
-DB_PORT = os.getenv("POSTGRES_PORT")
-DB_NAME = os.getenv("POSTGRES_DB")
-DB_USER = os.getenv("POSTGRES_USER")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("PGHOST")
+DB_PORT = os.getenv("PGPORT")
+DB_NAME = os.getenv("PGDATABASE")
+DB_USER = os.getenv("PGUSER")
+DB_PASSWORD = os.getenv("PGPASSWORD")
 
 # Conectar a la base de datos
 def get_db_connection():
